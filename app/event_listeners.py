3f1c6f0da6272488c769c3620a85f6c029775705
BlakeXYZ <37947050+BlakeXYZ@users.noreply.github.com>
2025-02-02 23:11:35 -0600

    Manually calling update_driver_event_stats -- issue with event_listeners

--- app/event_listeners.py
+++ app/event_listeners.py
@@ -1,28 +1,10 @@
+from datetime import datetime, timezone, timedelta
+
 from sqlalchemy import event
-import datetime
 
 from app import db
 from app.models import Laptime, DriverEvent, DriverEventStats
 
-# def update_driver_event_stats(driver_id, event_id):
-#     print(f"Updating stats for driver {driver_id} at event {event_id}")
-#     laptimes = db.session.query(Laptime).filter_by(driver_id=driver_id, event_id=event_id).all()
-#     total_runs = len(laptimes)
-#     total_time = sum((lt.laptime for lt in laptimes), datetime.timedelta())
-#     avg_laptime = total_time / total_runs if total_runs > 0 else None
-#     min_laptime = min((lt.laptime for lt in laptimes), default=None)
-#     max_laptime = max((lt.laptime for lt in laptimes), default=None)
-
-#     driver_event = db.session.query(DriverEvent).filter_by(driver_id=driver_id, event_id=event_id).first()
-#     if driver_event:
-#         if not driver_event.stats:
-#             driver_event.stats = DriverEventStats(driver_event=driver_event.id)
-#         driver_event.stats.avg_laptime = avg_laptime
-#         driver_event.stats.total_runs = total_runs
-#         driver_event.stats.min_laptime = min_laptime
-#         driver_event.stats.max_laptime = max_laptime
-#         driver_event.stats.total_time = total_time
-#         db.session.commit()
 
 def update_driver_event_stats(target):
     print("-"*20)
@@ -30,18 +12,20 @@ def update_driver_event_stats(target):
     print(f"Driver Event: {target.driver_event}")
 
     laptimes = db.session.query(Laptime).filter_by(driver_event=target.driver_event).all()
+    this_laps_driverEventStats = db.session.query(DriverEventStats).filter_by(driver_event=target.driver_event).first()
     total_runs = len(laptimes)
-    total_time = sum((lt.laptime for lt in laptimes), datetime.timedelta())
+    total_time = sum((lt.laptime for lt in laptimes), timedelta())
     avg_laptime = total_time / total_runs if total_runs > 0 else None
     min_laptime = min((lt.laptime for lt in laptimes), default=None)
     max_laptime = max((lt.laptime for lt in laptimes), default=None)
 
     print(f"Total Runs: {total_runs}")
+    print(f"Total Time: {total_time}")
+    print(f"Average Laptime: {avg_laptime}")
 
 
-
-    if not target.driver_event.driver_event_stats:
-        print(f"Creating new DriverEventStats for {target.driver_event}")
+    if this_laps_driverEventStats is None:
+        print(f"Creating new DriverEventStats for {target.driver_event} -------")
 
         set_driver_event_stats = DriverEventStats(
             driver_event_id=target.driver_event.id,
@@ -49,20 +33,30 @@ def update_driver_event_stats(target):
             average_lap=avg_laptime,
             total_laps=total_runs
             )
+        
+        db.session.add(set_driver_event_stats)
 
-    # set_driver_event_stats.fastest_lap = min_laptime
-    # set_driver_event_stats.average_lap = avg_laptime
-    # set_driver_event_stats.total_laps = total_runs
-    db.session.add(set_driver_event_stats)
+    if this_laps_driverEventStats:
 
-    print("="*80)
-    print("="*80)
+        print(f"Updating existing DriverEventStats for {this_laps_driverEventStats} -------")
+        print(f"total laps before update: {this_laps_driverEventStats.total_laps}")
+
+
+        this_laps_driverEventStats.total_laps = total_runs
+        db.session.merge(this_laps_driverEventStats)  # Merge to avoid overriding the session state
+
+        print(f"total laps after update: {this_laps_driverEventStats.total_laps}")
+
+
+
+
+    
 
 
 # @event.listens_for(Laptime, 'after_insert')
 # def after_insert_laptime(mapper, connection, target):
 #     print(f"Running Event Listener AFTER INSERT for {target}")
-#     update_driver_event_stats(target)
+#     add_driver_event_stats(target)
 
 # @event.listens_for(Laptime, 'after_update')
 # def after_update_laptime(mapper, connection, target):
@@ -74,12 +68,13 @@ def update_driver_event_stats(target):
 #     print(f"Running Event Listener AFTER DELETE for {target}")
 #     update_driver_event_stats(target)
 
-@event.listens_for(db.session, 'after_flush')
-def after_flush(session, context):
-    for instance in session.new:
-        if isinstance(instance, Laptime):
-            print(f"Running Event Listener AFTER FLUSH for {instance}")
-            update_driver_event_stats(instance)
+# @event.listens_for(db.session, 'after_flush')
+# def after_flush(session, context):
+#     print(f"New instances in session: {session.new}")
+#     for instance in session.new:
+#         if isinstance(instance, Laptime):
+#             print(f"Running Event Listener AFTER FLUSH for {instance}")
+#             update_driver_event_stats(instance)
 
 
 
--- tests.py
+++ tests.py
@@ -8,6 +8,51 @@ class TestConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = 'sqlite://'
 
+def update_driver_event_stats(target):
+    print("-"*20)
+    print(f"Running Event Listener for {target}")
+    print(f"Driver Event: {target.driver_event}")
+
+    laptimes = db.session.query(Laptime).filter_by(driver_event=target.driver_event).all()
+    this_laps_driverEventStats = db.session.query(DriverEventStats).filter_by(driver_event=target.driver_event).first()
+    total_runs = len(laptimes)
+    total_time = sum((lt.laptime for lt in laptimes), timedelta())
+    avg_laptime = total_time / total_runs if total_runs > 0 else None
+    min_laptime = min((lt.laptime for lt in laptimes), default=None)
+    max_laptime = max((lt.laptime for lt in laptimes), default=None)
+
+    print(f"Total Runs: {total_runs}")
+    print(f"Total Time: {total_time}")
+    print(f"Average Laptime: {avg_laptime}")
+
+
+    if this_laps_driverEventStats is None:
+        print(f"Creating new DriverEventStats for {target.driver_event} -------")
+
+        set_driver_event_stats = DriverEventStats(
+            driver_event_id=target.driver_event.id,
+            fastest_lap=min_laptime,
+            average_lap=avg_laptime,
+            total_laps=total_runs
+            )
+        
+        db.session.add(set_driver_event_stats)
+
+    if this_laps_driverEventStats:
+
+        print(f"Updating existing DriverEventStats for {this_laps_driverEventStats} -------")
+        print(f"total laps before update: {this_laps_driverEventStats.total_laps}")
+
+
+        this_laps_driverEventStats.total_laps = total_runs
+
+        print(f"total laps after update: {this_laps_driverEventStats.total_laps}")
+
+
+
+
+# def hello_world():
+#     print("Hello World")
 
 class DriverEventModelCase(unittest.TestCase):
     def setUp(self):
@@ -27,26 +72,38 @@ class DriverEventModelCase(unittest.TestCase):
         self.assertEqual(d.driver_name, 'test_driver')
 
     def test_lap(self):
+        print(f"UNITTEST - Running test_lap ------------------------ ")
         d = Driver(driver_name='Joe Bobby')
         e = Event(event_name='Grand Prix', event_date=datetime.now(timezone.utc))
         c = Car(car_name='Mercedes', car_class='S Class')
         de = DriverEvent(driver=d, event=e, car=c)
         db.session.add_all([d, e, c, de])
         db.session.commit()
-        
 
         l = Laptime(driver_event=de, laptime=timedelta(minutes=1, seconds=30), run_number=1)
         db.session.add(l)
+        update_driver_event_stats(l)
         db.session.commit()
+        print(f"session committed")
         stats = db.session.query(DriverEventStats).filter_by(driver_event=de).first()
         print(f"Stats: {stats}")
-            
-                        
 
-        l_2 = Laptime(driver_event=de, laptime=timedelta(minutes=1, seconds=40), run_number=2)
-        db.session.add(l_2)
+        #query all laps
+        laptimes = db.session.query(Laptime).filter_by(driver_event=de).all()
+        print(f"Printing all laptimes: {laptimes}")          
+
+        print(f"ADDING SECOND LAP -------------------------------------------------------")
+        l_2 = Laptime(driver_event=de, laptime=timedelta(minutes=3, seconds=30), run_number=2)
+        db.session.add(l_2)  
+        update_driver_event_stats(l_2)
         db.session.commit()
+        print(f"session committed")
+        stats = db.session.query(DriverEventStats).filter_by(driver_event=de).all()
+        print(f"New Stats after adding 2nd lap: {stats}")
 
+        #query all laps
+        laptimes = db.session.query(Laptime).filter_by(driver_event=de).all()
+        print(f"Printing all laptimes: {laptimes}")
 
 if __name__ == '__main__':
     # unittest.main(verbosity=2)
