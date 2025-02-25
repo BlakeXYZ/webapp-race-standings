66b0140e245e20821694a656b4263798063af808
BlakeXYZ <37947050+BlakeXYZ@users.noreply.github.com>
2025-02-24 23:16:42 -0600

    bootstrap table - set to borderless

--- app/static/js/my-bootstrap-tables.js
+++ app/static/js/my-bootstrap-tables.js
@@ -1,6 +1,8 @@
 
+// using plugin bootstrap-table - docs: https://bootstrap-table.com/docs/getting-started/introduction/
+
 $(document).ready(function() {
-    $('#my-event-bootstrap-table').bootstrapTable({
+    $('.my-event-bootstrap-table').bootstrapTable({
         cache: false,
         sortName:"my-driver",
         sortOrder:"asc",
@@ -8,16 +10,14 @@ $(document).ready(function() {
         pagination: true,
         pageSize: 10, //specify 5 here
         pageList: "[10, 20, 50, all]",//list can be specified here
-        search: true // Enable search functionality
+        search: true, // Enable search functionality
+        classes: 'table table-borderless table-hover table-striped'
     });
-
-    // Add multiple classes to the table
-    $('#my-event-bootstrap-table').addClass('table table-borderless table-hover table-striped');
 
     $("<style>")
         .prop("type", "text/css")
         .html(".fixed-table-container { padding-bottom: 25px !important; }")
         .appendTo("head");
-
-
 });
+
+
